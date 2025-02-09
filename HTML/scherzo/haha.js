a6f0211a98f109a5f3b3a3ab6be0c2088c14ed25
Albert742 <nomes8568@gmail.com>
2025-02-09 15:46:53 +0100

    09/02

--- HTML/scherzo/haha.js
+++ HTML/scherzo/haha.js
@@ -1,26 +1,9 @@
-function getrolled() {
-    setTimeout(function() {
-        window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-    }, 5000);
-};
-
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('bottone');
     const alertSound = document.getElementById('alertSound');
     let tries = 0;
     const maxTries = 20;
-
-    button.addEventListener('click', function() {
-        if (tries >= maxTries) {
-            alertSound.play();
-            alert("Va bene, hai vinto!!! (\u256F\u00B0\u25A1\u00B0)\u256F\uFE35 \u253B\u2501\u253B");
-            setTimeout(function() {
-                window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-            }, 800);
-        } else {
-            getrolled();
-        }
-    });
+    const content = document.querySelector('.content'); // Ottieni il contenitore
 
     document.addEventListener('mousemove', function(event) {
         if (tries >= maxTries) return;
@@ -30,13 +13,29 @@ document.addEventListener('DOMContentLoaded', function() {
         const distanceY = Math.abs(event.clientY - (rect.top + rect.height / 2));
 
         if (distanceX < 150 && distanceY < 150) {
-            let x = Math.random() * (window.innerWidth - button.clientWidth);
-            let y = Math.random() * (window.innerHeight - button.clientHeight);
+            // Calcola le nuove posizioni X e Y all'interno del contenitore
+            const contentRect = content.getBoundingClientRect();
+            let x = Math.random() * (contentRect.width - button.clientWidth);
+            let y = Math.random() * (contentRect.height - button.clientHeight);
+
+            // Imposta le nuove posizioni
             button.style.position = 'absolute';
             button.style.left = x + 'px';
             button.style.top = y + 'px';
             tries++;
         }
         console.log('Tentativi', tries )
+    });
+
+    button.addEventListener('click', function() {
+        if (tries >= maxTries) {
+            alertSound.play();
+            alertSound.onended = function() {
+                alert("Va bene, hai vinto!!! (\u256F\u00B0\u25A1\u00B0)\u256F\uFE35 \u253B\u2501\u253B");
+                window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+            };
+        } else {
+            getrolled();
+        }
     });
 });
